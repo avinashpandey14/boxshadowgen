0307d9e3a361a0f3c60c9da558e1d0eb3d399e68
avinashpandey14 <pandeyavinash705@gmail.com>
2021-05-11 07:20:39 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [Hori, setHori] = useState(10)
+  const [Veri, setVeri] = useState(10)
+  const [Blur, setBlur] = useState(10)
+  const [Color, setColor] = useState("black")
+  const [checkOn,setCheckon] = useState(false)
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="controls">
+        <label>Horizontal length</label>
+        <input type="range" min="-200" max="200" value={Hori} onChange={(e) => setHori(e.target.value)} />
+        <label>Vertical length</label>
+        <input type="range" min="-200" max="200" value={Veri} onChange={(e) => setVeri(e.target.value)} />
+        <label>Blur</label>
+        <input type="range" min="0" max="200" value={Blur} onChange={(e) => setBlur(e.target.value)} />
+        <label>Color</label>
+        <input type="color" value={Color} onChange={(e) => setColor(e.target.value)} />
+        <div class="switch">
+          <label>
+            Outline
+      <input type="checkbox" checked={checkOn} onChange={() => setCheckon(!checkOn)}/>
+              <span className="lever"></span>
+      inset
+    </label>
     </div>
+        </div>
+        <div className="output">
+          <div className="box" style={{ boxShadow: ` ${checkOn?"inset" :""} ${Hori}px ${Veri}px ${Blur}px black ` }}>
+            <p>{Hori}px {Veri}px {Blur}px {Color}</p>
+          </div>
+        </div>
+      </div>
   );
 }
 
